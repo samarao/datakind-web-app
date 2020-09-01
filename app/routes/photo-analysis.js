cfd5bc41866c26a95df6cf415566c53e853bfed6
Sama <samarao@salesforce.com>
2020-09-01 15:07:37 -0700

    Add Procfile & fix styling

--- app/routes/photo-analysis.js
+++ app/routes/photo-analysis.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
+    let animalId = sessionStorage.getItem("animalId") || '1234';
     const animal = this.store.findAll('animal', {});
     return animal;
   }
